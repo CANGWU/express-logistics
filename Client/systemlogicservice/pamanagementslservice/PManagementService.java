10867b4ba783a7909a5843e468334f0e69b71968
CANGWU <CANGWU@users.noreply.github.com>
2015-11-04 22:43:08 +0800

    Update PManagementService.java

--- Client/systemlogicservice/pamanagementslservice/PManagementService.java
+++ Client/systemlogicservice/pamanagementslservice/PManagementService.java
@@ -8,7 +8,7 @@ public interface PManagementService {
 	
 	public StaffVO select(String id);
 	public void delect(String id);
-	public void revise(String id);
+	public StaffVO revise(String id);
 	public void saveChange(StaffVO vo);
 	public void save(StaffVO vo);
 	public void add();
