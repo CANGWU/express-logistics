a9ab480e282b67bb0c7b7a3a78f0d04186706488
CANGWU <1533704796@qq.COM>
2015-11-12 08:30:55 +0800

    接口修改

--- Client/systemlogicservice/strategyslservice/SalaryStrategyService.java
+++ Client/systemlogicservice/strategyslservice/SalaryStrategyService.java
@@ -7,8 +7,12 @@ import vo.SalaryVO;
 public interface SalaryStrategyService {
 	
 	public ArrayList<SalaryVO> getSalaryStrategy();
-    public void endSalaryStrategy();
-    public void save(SalaryVO vo);
-    public void update(SalaryVO vo);
+        public void endSalaryStrategy();
+        public void save(SalaryVO vo);
+        public void saveChange(SalaryVO vo);
 	public void newSalaryVO(); 
+        public SalaryVO select(String id);
+        public void revise();
+     
+    
 }
